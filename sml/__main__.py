cf22f728cfce24054e703b9d25f120496601f15d
zqamemz <zqamemz@163.com>
2026-07-19 05:41:04 +0800

    feat: 添加 GNU GPLv3 协议

--- sml/__main__.py
+++ sml/__main__.py
@@ -2,11 +2,20 @@
 
 在 SSH 和真实命令行环境下直接使用的 ME Frp 管理器。
 
-用法:
-  sml                  # 启动 TUI
-  sml install          # 安装 mefrpc
-  sml install --force  # 强制重装
-  sml uninstall        # 卸载 mefrpc
+Copyright (C) 2025  zqamemz
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import sys
